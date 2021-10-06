735af3d39c94dddf59b6bc3558676d85a93da9cc
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-06 12:20:15 +0100

    add: added dynamic html templates

--- base/views.py
+++ base/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {
+        'id': 1,
+        'name': 'Lets learn python'
+    },
+    {
+        'id': 2,
+        'name': 'maths is great'
+    },
+    {
+        'id': 3,
+        'name': 'java'
+    },
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    template = 'base/home.html'
+    return render(request, template, context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for x in rooms:
+        if x['id'] == int(pk):
+            room = i
+    
+    context = {'room': room}
+
+    template = 'base/room.html'
+    return render(request, template, context)
