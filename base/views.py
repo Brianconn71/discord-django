a265d68084a4eb7119f6e3913f690c7d6ea5e843
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-07 12:59:48 +0100

    add: added room model and set up views to get toom info from db

--- base/views.py
+++ base/views.py
@@ -1,34 +1,32 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {
-        'id': 1,
-        'name': 'Lets learn python'
-    },
-    {
-        'id': 2,
-        'name': 'maths is great'
-    },
-    {
-        'id': 3,
-        'name': 'java'
-    },
-]
+# rooms = [
+#     {
+#         'id': 1,
+#         'name': 'Lets learn python'
+#     },
+#     {
+#         'id': 2,
+#         'name': 'maths is great'
+#     },
+#     {
+#         'id': 3,
+#         'name': 'java'
+#     },
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     template = 'base/home.html'
     return render(request, template, context)
 
 
 def room(request, pk):
-    room = None
-    for x in rooms:
-        if x['id'] == int(pk):
-            room = i
-    
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     template = 'base/room.html'
