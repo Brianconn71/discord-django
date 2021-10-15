b0da5eb6896f3dc6ea4f32257313fa5454a29331
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-15 21:34:07 +0100

    finished off crud functionality for the site

--- base/views.py
+++ base/views.py
@@ -46,3 +46,24 @@ def createRoom(request):
     template = 'base/room_form.html'
     return render(request, template, context)
 
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == "POST":
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form }
+    template = 'base/room_form.html'
+    return render(request, template, context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    template = 'base/delete.html'
+    return render(request, template, {'obj': room})
