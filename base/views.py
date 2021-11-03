588b76ee7aa17f0281c29d07df89760b8d3d96db
Brian Connolly <Brian.connolly71@gmail.com>
2021-11-03 12:11:17 +0000

    add: added static files set up and edited profile page

--- base/views.py
+++ base/views.py
@@ -126,9 +126,14 @@ def room(request, pk):
     return render(request, template, context)
 
 
-def userProfile(request):
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+
     template = 'base/profile.html'
-    context = {}
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
 
     return render(request, template, context)
 
